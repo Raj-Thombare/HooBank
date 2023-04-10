2c8766820e7e2595e3ddba130e239fcd2cb1a26c
Raj Thombare <therajthombare@gmail.com>
2023-04-11 02:02:09 +0530

    add components

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,44 @@
+import React from 'react'
+import {
+  Navbar,
+  Hero,
+  Billing,
+  Business,
+  CardDeal,
+  Clients,
+  Footer,
+  CTA,
+  Stats,
+  Testimonials,
+} from "./components";
 
-function App() {
-
+const App = () => {
   return (
-    <div className="App">
-      
+    <div className="bg-primary w-full overflow-hidden">
+      <div className="sm:px-16 px-6 flex justify-center items-center">
+        <div className="xl:max-w-[1280px] w-full">
+          <Navbar />
+        </div>
+      </div>
+
+      <div className="bg-black flex justify-center items-start">
+        <div className="xl:max-w-[1280px] w-full"><Hero /></div>
+      </div>
+
+      <div className="bg-black sm:px-16 px-6 flex justify-center items-start">
+        <div className="xl:max-w-[1280px] w-full">
+          <Stats />
+          <Business />
+          <Billing />
+          <CardDeal />
+          <Testimonials />
+          <Clients />
+          <CTA />
+          <Footer />
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
 export default App
---
+++ src/components/index.ts
@@ -0,0 +1,23 @@
+import Navbar from "./Navbar";
+import Hero from "./Hero";
+import Billing from "./Billing";
+import Business from "./Business";
+import CardDeal from "./CardDeal";
+import Clients from "./Clients";
+import Footer from "./Footer";
+import CTA from "./CTA";
+import Stats from "./Stats";
+import Testimonials from "./Testimonials";
+
+export {
+  Navbar,
+  Hero,
+  Billing,
+  Business,
+  CardDeal,
+  Clients,
+  Footer,
+  CTA,
+  Stats,
+  Testimonials,
+};
