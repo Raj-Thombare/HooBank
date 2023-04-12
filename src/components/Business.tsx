45425256920b1f792f63279b386099cdb8e1446c
Raj Thombare <therajthombare@gmail.com>
2023-04-12 22:46:32 +0530

    business section completed

--- src/components/Business.tsx
+++ src/components/Business.tsx
@@ -1,4 +1,32 @@
 import { features } from "../constants";
+import Button from "./Button";
+
+type Props = {
+  icon:string,
+  title:string,
+  content:string,
+  index:number
+};
+
+const FeatureCard: React.FC<Props> = ({ icon, title, content, index }) => (
+  <div
+    className={`flex flex-row p-6 rounded-[20px] ${
+      index !== features.length - 1 ? "mb-6" : "mb-0"
+    } feature-card`}
+  >
+    <div className="w-[64px] h-[64px] rounded-full flex justify-center items-center bg-dimBlue">
+      <img src={icon} alt="icon" className="w-[50%] h-[50%] object-contain" />
+    </div>
+    <div className="flex-1 flex flex-col ml-3">
+      <h4 className="font-poppins font-semibold text-white text-[18px] leading-[23.4px] mb-1">
+        {title}
+      </h4>
+      <p className="font-poppins font-normal text-dimWhite text-[16px] leading-[24px]">
+        {content}
+      </p>
+    </div>
+  </div>
+);
 
 const Business: React.FC = () => {
   return (
@@ -13,6 +41,14 @@ const Business: React.FC = () => {
           building credit, earning rewards and saving money. But with hundreds
           of credit cards on the market.
         </p>
+
+        <Button styles="mt-10" />
+      </div>
+
+      <div className="flex-1 flex ${styles.flexCenter} md:ml-10 ml-0 md:mt-0 mt-10 relative flex-col">
+        {features.map((feature, index) => (
+          <FeatureCard key={feature.id} {...feature} index={index} />
+        ))}
       </div>
     </section>
   );
