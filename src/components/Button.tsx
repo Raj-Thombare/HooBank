60d0a2084a19cdb5170338c73a6820105406b9df
Raj Thombare <therajthombare@gmail.com>
2023-04-12 22:28:26 +0530

    add Button component

---
+++ src/components/Button.tsx
@@ -0,0 +1,16 @@
+type Props = {
+  styles: string;
+};
+
+const Button: React.FC<Props> = ({ styles }) => {
+  return (
+    <button
+      type="button"
+      className={`py-4 px-6 bg-blue-gradient font-poppins font-medium text-[18px] text-primary outline-none ${styles}`}
+    >
+      Get Started
+    </button>
+  );
+};
+
+export default Button;
