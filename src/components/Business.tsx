ebc5b21c7c50d42cf3a1432f64b35c7276e4d32b
Raj Thombare <therajthombare@gmail.com>
2023-04-12 22:24:08 +0530

    add business section heading & info

--- src/components/Business.tsx
+++ src/components/Business.tsx
@@ -1,7 +1,21 @@
-import React from "react";
+import { features } from "../constants";
 
 const Business: React.FC = () => {
-  return <div>Business</div>;
+  return (
+    <section id="features" className="flex md:flex-row flex-col sm:py-16 py-6">
+      <div className="flex-1 flex justify-center items-start flex-col">
+        <h2 className="font-poppins font-semibold xs:text-[48px] text-[40px] text-white xs:leading-[76.8px] leading-[66.8px] w-full">
+          you do the business, <br className="sm:block hidden" /> we'll handle
+          the money.
+        </h2>
+        <p className="font-poppins font-normal text-dimWhite text-[18px] leading-[30.8px] max-w-[470px] mt-5">
+          With the right credit card, you can improve your financial life by
+          building credit, earning rewards and saving money. But with hundreds
+          of credit cards on the market.
+        </p>
+      </div>
+    </section>
+  );
 };
 
 export default Business;
