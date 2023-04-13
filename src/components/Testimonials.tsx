ef1443ba1cf9d34d42d9804f9a535fde0546130c
Raj Thombare <therajthombare@gmail.com>
2023-04-13 06:24:42 +0530

    testimonials section completed

--- src/components/Testimonials.tsx
+++ src/components/Testimonials.tsx
@@ -7,7 +7,7 @@ const Testimonials: React.FC = () => {
       id="clients"
       className="sm:py-16 py-6 flex justify-center items-center flex-col relative"
     >
-      <div />
+      <div className="absolute z-[0] w-[60%] h-[60%] -right-[50%] rounded-full blue__gradient"></div>
 
       <div className="w-full flex justify-between items-center md:flex-row flex-col sm:mb-16 mb-6 relative z-[1]">
         <h1 className="font-poppins font-semibold xs:text-[48px] text-[40px] text-white xs:leading-[76.8px] leading-[66.8px] w-full">
