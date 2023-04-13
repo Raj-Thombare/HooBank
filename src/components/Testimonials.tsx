0d0455bd8dd7adcc987081c4441e7c53dcdc1fbc
Raj Thombare <therajthombare@gmail.com>
2023-04-13 05:53:57 +0530

    add testimonials

--- src/components/Testimonials.tsx
+++ src/components/Testimonials.tsx
@@ -1,7 +1,28 @@
-import React from "react";
-
 const Testimonials: React.FC = () => {
-  return <div>Testimonials</div>;
+  return (
+    <section
+      id="clients"
+      className="sm:py-16 py-6 flex justify-center items-center flex-col relative"
+    >
+      <div />
+
+      <div className="w-full flex justify-between items-center md:flex-row flex-col sm:mb-16 mb-6 relative z-[1]">
+        <h1 className="font-poppins font-semibold xs:text-[48px] text-[40px] text-white xs:leading-[76.8px] leading-[66.8px] w-full">
+          What people are <br className="sm:block hidden" /> saying about us
+        </h1>
+        <div className="w-full md:mt-0 mt-6">
+          <p className="font-poppins font-normal text-dimWhite text-[18px] leading-[30.8px] text-left max-w-[450px]">
+            Everything you need to accept card payments and grow your business
+            anywherer on the planet.
+          </p>
+        </div>
+      </div>
+
+      <div className="flex flex-wrap sm:justify-start justify-center w-full feedback-container relative z-[1]">
+
+      </div>
+    </section>
+  );
 };
 
 export default Testimonials;
